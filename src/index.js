15efb43e67672c268fdaf8d945b2db286a228074
Kat Tannehill <katetannehill@gmail.com>
2023-09-20 08:25:21 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// GET all dog data from server
+    // Create a button in dog bar with each dog's name
+    // Add event to button, when clicked displays dog's info 
+        // picture, name, good/ bad dog button
+    // Add event to bad dog button to toggle between good/ bad
+    // Add event to filter good dogs when clicked
+        // shows only good dog names in dog bar
+
+
+
+// Global variables
+
+
+
+// Helper functions
+
+
+
+// Fetch requests
+
+
+
